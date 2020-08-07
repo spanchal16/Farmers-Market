e53e32e9fe3c1e20e9682a10981e68a2664337bb
Nawaphan Chayopathum <chayopat@sheridanc.on.ca>
2020-08-07 17:46:12 -0300

    CRUD for products table with UI

--- farmersCompany/api/controllers/ProductsController.js
+++ farmersCompany/api/controllers/ProductsController.js
@@ -5,62 +5,162 @@ function showError(code, message, res) {
         code: code,
         message: message
     }
-    res.view("pages/error", { error: error });
-}
-
-function showErrorPart(partId, res) {
-    let code = "400";
-    let message = "partId: " + partId + " does not exist.";
-    showError(code, message, res);
+    res.view("error", { err: error });
 }
 
 module.exports = {
     // GET ALL
     viewData: function (req, res) {
-        const sqlSelectAll = `SELECT product, price FROM products`
-        sails.sendNativeQuery(sqlSelectAll, function (err, rawResult) {
-            console.log(rawResult.rows);
-            return res.json(rawResult.rows);
-/*             let jobs = [];
-            for (let [key, value] of Object.entries(rawResult.rows)) {
-                let job = {};
-                for (let [k, v] of Object.entries(value)) {
-                    job[k] = v;
+        const sqlSelectAll = `SELECT product, price, productID FROM products`;
+        try {
+            sails.sendNativeQuery(sqlSelectAll, function (err, rawResult) {
+                if (!rawResult.rows) {
+                    let code = "400";
+                    let message = "No data in database";
+                    showError(code, message, res);
                 }
-                jobs.push(job);
-            }
-            if (!jobs) {
-                res.send("Cannot find anything to show!")
-            }
-            if (jobs) {
-                res.view("pages/homepage", { jobs: jobs })
-            } */
-        }); 
+                if (rawResult.rows) {
+                    return res.view("pages/homepage", { products: rawResult.rows })
+                }
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
     },
-    // GET ONE BY ID
-    viewDataByID: async function (req, res) {
-        const jobName = req.param('jobName');
-        const partId = parseInt(req.param('partId'));
-        const sqlSelectOne = `SELECT * FROM jobs WHERE jobName = $1 AND partId = $2`;
+    // Search product by ID
+    searchProduct: async function (req, res) {
+        const productID = parseInt(req.body.productID);
 
-        await sails.sendNativeQuery(sqlSelectOne, [jobName, partId], function (err, rawResult) {
-            var length = rawResult.rows.length;
-            if (length == 0) {
-                let code = "400";
-                let message = "jobName: " + jobName + " with " + "partId: " + partId + " do not exist, can't retrieve data.";
-                showError(code, message, res);
-            } else {
-                var job = {};
-                for (let [key, value] of Object.entries(rawResult.rows)) {
-                    for (let [k, v] of Object.entries(value)) {
-                        job[k] = v;
+        try {
+            const sqlSelectOne = `SELECT * FROM products WHERE productID = $1`;
+            await sails.sendNativeQuery(sqlSelectOne, [productID], function (err, rawResult) {
+                var length = rawResult.rows.length;
+                if (length == 0) {
+                    // let code = "400";
+                    // let message = "Product ID: " + productID + partId + " do not exist, can't retrieve data.";
+                    // showError(code, message, res);
+                    res.view("pages/products/searchProduct", { product: null });
+                } else {
+                    var product = {};
+                    for (let [key, value] of Object.entries(rawResult.rows)) {
+                        for (let [k, v] of Object.entries(value)) {
+                            product[k] = v;
+                        }
                     }
+                    res.view("pages/products/searchProduct", { product: product });
                 }
-                res.view("pages/jobs/viewDataByID", { job: job });
-            }
-        });
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
+
+
+    },
+    // Add a product
+    addProduct: async function (req, res) {
+        const productID = parseInt(req.body.productID);
+        const productName = req.body.productName;
+        const stock = parseInt(req.body.stock);
+        const price = parseFloat(req.body.price);
+
+        try {
+            const sqlSelectOne = `SELECT * FROM products WHERE productID = $1`;
+            await sails.sendNativeQuery(sqlSelectOne, [productID], async function (err, rawResult) {
+                var length = rawResult.rows.length;
+                if (length != 0) {
+                    let code = "400";
+                    let message = "Product ID: " + productID + " already exist, can't add a product";
+                    showError(code, message, res);
+                } else {
+                    const sqlInsert = `INSERT INTO products VALUES ($1, $2, $3, $4)`;
+                    await sails.sendNativeQuery(sqlInsert, [productID, productName, stock, price]);
+                    res.redirect("/");
+                }
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
+    },
+    // Fetch existing product
+    editProduct: async function (req, res) {
+        const productID = req.params.productID;
+        const sqlSelectOne = `SELECT * FROM products WHERE productID = $1`;
+
+        try {
+            await sails.sendNativeQuery(sqlSelectOne, [productID], function (err, rawResult) {
+                console.log(rawResult.rows);
+                var length = rawResult.rows.length;
+                if (length == 0) {
+                    let code = "400";
+                    let message = "Product ID: " + productID + " do not exist, can't retrieve data.";
+                    showError(code, message, res);
+                } else {
+                    var product = {};
+                    for (let [key, value] of Object.entries(rawResult.rows)) {
+                        for (let [k, v] of Object.entries(value)) {
+                            product[k] = v;
+                        }
+                    }
+                    return res.view("pages/products/updateProduct", { product: product });
+                }
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
     },
 
+    // Update product
+    updateProduct: function (req, res) {
+        const productID = parseInt(req.body.productID);
+        const productName = req.body.productName;
+        const stock = parseInt(req.body.stock);
+        const price = parseFloat(req.body.price);
+
+        const sqlSelectOne = `SELECT * FROM products WHERE productID = $1`;
+
+        try {
+            sails.sendNativeQuery(sqlSelectOne, [productID], async function (err, rawResult) {
+                var length = rawResult.rows.length;
+                if (length != 0) {
+                    const sqlUpdate = `UPDATE products SET product = $1, stock = $2, price = $3 WHERE productID = $4`;
+                    await sails.sendNativeQuery(sqlUpdate, [productName, stock, price, productID]);
+                    return res.redirect("/");
+
+                } else {
+                    let code = "400";
+                    let message = "Product ID: " + productID + " do not exist, can't update data";
+                    showError(code, message, res);
+                }
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
+    },
+
+    // DELETE DATA
+    deleteProduct: async function (req, res) {
+        const productID = parseInt(req.body.productID);
+        const productName = req.body.productName;
+        try {
+            const sqlSelectOne = `SELECT * FROM products WHERE productID = $1 AND product = $2`;
+            sails.sendNativeQuery(sqlSelectOne, [productID, productName], async function (err, rawResult) {
+                var length = rawResult.rows.length;
+                if (length != 0) {
+                    const sqlDelete = `DELETE FROM products WHERE productID = $1 AND product = $2`;
+                    await sails.sendNativeQuery(sqlDelete, [productID, productName]);
+                    return res.redirect("/");
+                } else {
+                    let code = "400";
+                    let message = "Product ID: " + productID + " do not exist, can't delete data";
+                    showError(code, message, res);
+                }
+            })
+        } catch (err) {
+            return res.view("error", { err: err });
+        }
+    },
+
+    // API
     getallProducts: async function (req, res) {
 
         const sql = `SELECT productID, product FROM products`;
@@ -75,7 +175,7 @@ module.exports = {
         sails.log(data.rows);
         return res.json(data.rows);
     },
-    
+
     buyProduct: async function (req, res) {
         console.log(req.params);
         const productID = parseInt(req.params.productID);
@@ -85,32 +185,32 @@ module.exports = {
         const sql = `SELECT stock FROM products WHERE productID = $1 AND product = $2`;
         await sails.sendNativeQuery(sql, [productID, product], async function (err, rawResult) {
             var length = rawResult.rows.length;
-            
+
             if (length != 0) {
                 stock = rawResult.rows[0].stock
                 console.log(amount)
                 console.log(stock)
-                if(amount <= stock){
-                    newStock= stock - amount;
+                if (amount <= stock) {
+                    newStock = stock - amount;
                     const sqlUpdate = `UPDATE products SET stock= $1 WHERE productID = $2 AND product = $3`;
                     try {
                         await sails.sendNativeQuery(sqlUpdate, [newStock, productID, product]);
                         sails.log("stock updated");
-                        return res.json({status: 'success'});
+                        return res.json({ status: 'success' });
 
                     } catch (err) {
                         sails.log("not able to add update stock in products");
-                        return res.json({status: 'unsuccessful'});
+                        return res.json({ status: 'unsuccessful' });
 
                     }
 
                 }
-                else{
-                    return res.json({status: 'not enough stock'})
+                else {
+                    return res.json({ status: 'not enough stock' })
                 }
-            
+
             } else {
-                return res.json({status: 'unsuccessful'})
+                return res.json({ status: 'unsuccessful' })
 
             }
         });
--- farmersCompany/config/routes.js
+++ farmersCompany/config/routes.js
@@ -1,10 +1,27 @@
 module.exports.routes = {
   
-  "/": { view: "pages/homepage" },
-
   // Retrieve data
-  "GET /products/viewData": "ProductsController.viewData",
-  "GET /jobs/viewDataByID": "ProductsController.viewDataByID",
+  "/": { view: "pages/homepage" },
+  "GET /": "ProductsController.viewData",
+
+  // "GET /products/viewDataByID": "ProductsController.viewDataByID",
+
+  //Add a product
+  "GET /addProduct": { view: "pages/products/addProduct" },
+  "POST /addProduct": "ProductsController.addProduct",
+
+  // Search a product by ID
+  "GET /searchProduct": { view: "pages/products/searchProduct" },
+  "POST /searchProduct": "ProductsController.searchProduct",
+
+
+  // Update a product
+  "GET /updateProduct/:productID": "ProductsController.editProduct",
+  "POST /updateProduct": "ProductsController.updateProduct",
+
+  // Delete a product
+  "GET /deleteProduct": { view: "pages/products/deleteProduct" },
+  "POST /deleteProduct": "ProductsController.deleteProduct",
 
   // API routes
   "GET /api/getallProducts": "ProductsController.getallProducts",
@@ -15,4 +32,6 @@ module.exports.routes = {
   "POST /api/getOneJob": "JobsController.getOneJob",
   "POST /api/savePartOrders": "PartOrdersController.savePartOrders", */
 
+  "GET /error": { view: "error" },
+  "GET /notfound": { view: "notfound" },
 };
